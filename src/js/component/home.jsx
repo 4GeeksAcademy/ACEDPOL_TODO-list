f0c0e83cb2dfd47e0fffa2e8819b16ae90ada4cf
P. Pablo Cubells Talavera <pablocub@ucm.es>
2024-11-18 23:12:06 +0000

    bastante parecido al ejemplo: logica & apariencia

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [listTasks, setListTasks] = useState([]);
 
-//create your first component
-const Home = () => {
+	const handleOnChange = (e) => {
+		setInputValue(e.target.value);
+		// console.log(inputValue);
+	}
+
+	const saveTask = (e) => {
+		e.preventDefault();
+		setListTasks(prevState => [...prevState, inputValue.trim()])
+		setInputValue("")
+	}
+
+	const deleteTask = (taskToDelete) => {
+		// console.log(taskToDelete.trim())
+		setListTasks(listTasks.filter(task => task !== taskToDelete.trim()));
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container text-center w-25">
+			<h1 className="text-black text-opacity-25 fw-light" style={{ fontSize: "7.5rem" }}>todos</h1>
+			<form onSubmit={saveTask}>
+				<input
+					autoFocus={true}
+					className="border p-3 w-100 no-outline text-black text-opacity-50"
+					value={inputValue}
+					placeholder="What needs to be done?"
+					onChange={handleOnChange}
+				/>
+				{/* <button type='submit'>Enviar</button> */}
+			</form>
+			<ul className='list-group'>
+				{listTasks.map((task, index) => (
+					<li key={index} className='list-group-item rounded-0 border border-top-0 text-black text-opacity-50 text-start d-flex align-items-center task-item'>
+						{task}
+						<button onClick={() => deleteTask(task)} className='btn ms-auto text-black text-opacity-50 border-0 task-button'><i className="fa-solid fa-x"></i></button>
+					</li>
+				))}
+			</ul>
+			{listTasks.length !== 0 ?
+				<>
+				<div className="border border-top-0" style={{ height: "1.5rem" }}>
+					<span className="fs-6 fw-light text-black text-opacity-50 float-end pe-2">
+						{listTasks.length} item left
+					</span>
+				</div>
+				<div className="border border-top-0 mx-auto" style={{ width: "95%", height: "0.25rem" }} />
+				<div className="border border-top-0 mx-auto" style={{ width: "90%", height: "0.25rem" }} />
+				</>
+				: ""
+			}
 		</div>
 	);
-};
-
-export default Home;
+}
